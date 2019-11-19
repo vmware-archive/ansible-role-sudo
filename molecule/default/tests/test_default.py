67a38c587cea6918be508ed62806541b3068954b
Tom Scanlan <tscanlan@vmware.com>
2019-11-19 13:32:46 -0500

    test for sudo binary

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+    f = host.file('/usr/bin/sudo')
 
     assert f.exists
     assert f.user == 'root'
